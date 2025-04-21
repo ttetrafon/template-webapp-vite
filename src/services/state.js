d5e068cbdb3d24f75cb17fe4d92a6591b89af658
ttetrafon <info@ttetrafon.com>
2025-04-21 14:53:24 +0300

    ... missed from previous commit

--- src/services/state.js
+++ src/services/state.js
@@ -9,21 +9,31 @@ class State {
       State.instance = this;
     }
 
-    // this.#user = new User(crypto.randomUUID(), "", roles.VISITOR);
     this.#observables = {};
 
+    let userUuid = Math.random();
+    try {
+      userUuid = crypto.randomUUID();
+    } catch(err) {}
     this.createObservable(
-      generalNames.OBSERVABLE_USER,
-      new User(crypto.randomUUID(), "", roles.VISITOR)
+      generalNames.OBSERVABLE_USER.description,
+      new User(userUuid, "", roles.VISITOR)
     );
 
     return State.instance;
   }
 
+  /**
+   *
+   * @param {String} observable
+   * @param {Object} obj
+   */
   createObservable(observable, obj) {
     let onChange = (property, newValue) => {
       console.log(`Property '${property}' changed to:`, newValue, "... calling subscribers!");
-      Object.keys(this.#observables[observable].listeners).forEach(listener => listener(property, newValue));
+      Object.keys(this.#observables[observable].listeners).forEach(subscriber =>
+        this.#observables[observable].listeners[subscriber](subscriber, property, newValue)
+      );
     };
 
     let proxy = new Proxy(obj, {
@@ -54,13 +64,19 @@ class State {
     }
   }
 
+  /**
+   *
+   * @param {String} observable: the name of the object
+   * @param {String} subscriber: the name of the subscriber
+   * @param {Function} callback: the function called in the subscriber when the
+   */
   subscribeToObservable(observable, subscriber, callback) {
     if (this.#observables.hasOwnProperty(observable) && !this.#observables[observable].listeners.hasOwnProperty(subscriber)) {
       this.#observables[observable].listeners[subscriber] = callback;
     }
   }
 
-  unsubscribeFromObservable(id, subscriber) {
+  unsubscribeFromObservable(observable, subscriber) {
     if (this.#observables.hasOwnProperty(observable) && this.#observables[observable].listeners.hasOwnProperty(subscriber)) {
       delete this.#observables[observable].listeners[subscriber];
     }
