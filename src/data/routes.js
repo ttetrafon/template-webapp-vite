f3c834441dd9519c1202f38e71643fa164c98957
ttetrafon <info@ttetrafon.com>
2025-07-15 01:14:34 +0300

    navigation skips recreating existing path elements

--- src/data/routes.js
+++ src/data/routes.js
@@ -3,7 +3,6 @@ export const routes = {
     content: 'page-one',
     title: "Page 1",
     description: "This is page one!",
-    path: "/page-one",
     pathType: "WebPage",
     navData: {
       test: "this is some test nav data!"
@@ -13,7 +12,6 @@ export const routes = {
     content: "page-two",
     title: "Page 2",
     description: "This is page two!",
-    path: "/page-two",
     pathType: "WebPage",
     navData: {
       test: "this is some test nav data!"
@@ -23,21 +21,18 @@ export const routes = {
     content: "tab-one",
     title: "Tab 1",
     description: "This is tab one!",
-    path: "/tab-one",
     pathType: "WebPage"
   },
   'tab-two': {
     content: "tab-two",
     title: "Tab 2",
     description: "This is tab two!",
-    path: "/tab-two",
     pathType: "WebPage"
   },
   '404': {
     content: "page-404",
     title: "404 - Page not found",
     description: "The page you request was not found!",
-    path: "/404",
     pathType: "WebPage"
   }
 }
